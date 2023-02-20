8a5f09459f61883f9dacbae1d05b576280121f42
mion158 <111487792+mion158@users.noreply.github.com>
2023-02-20 15:01:17 +0000

    views and serializers

--- djuwu/product/views.py
+++ djuwu/product/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
+class LatestProductsList(APIView):
+    #overwrite normal get function
+    def get(self, reqest, format=None):
+        # first 4 products
+        products = Product.objects.all()[0:4]
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
