a497c2223d7566ae5ccb1663459ac4178371dc68
mion158 <111487792+mion158@users.noreply.github.com>
2023-02-21 15:02:26 +0000

    minor changes

--- vuwue/src/main.js
+++ vuwue/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
 
-createApp(App).mount('#app')
+axios.default.baseURL = 'http://192.168.0.38:8080'
+
+
+createApp(App).use(store).use(router, axios).mount('#app')
