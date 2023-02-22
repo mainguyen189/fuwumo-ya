ce20d1e27c244893edc64e2273aae99d32a86ce4
mion158 <111487792+mion158@users.noreply.github.com>
2023-02-22 20:52:34 +0000

    viewed products front page

--- vuwue/src/main.js
+++ vuwue/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import axios from 'axios'
+
+
+axios.default.baseURL = 'http://localhost:8080'
+
+
+createApp(App).use(store).use(router, axios).mount('#app')
