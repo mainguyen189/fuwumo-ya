a9474003a5634b887aa76afa64cfd0f32cb58ce9
mion158 <111487792+mion158@users.noreply.github.com>
2023-02-22 21:03:29 +0000

    created backend product view

--- djuwu/product/views.py
+++ djuwu/product/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import Http404
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -13,3 +14,17 @@ class LatestProductsList(APIView):
         products = Product.objects.all()[0:4]
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+
+class ProductDetail(APIView):
+    def get_object(self, category_slug, product_slug):
+        try:
+            return Product.objects.filter(category_slug=category_slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404
+
+    # overwrite get function
+    def get(self, request, category_slug, product_slug, format=None):
+        product = self.get_object(category_slug, product)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+        
