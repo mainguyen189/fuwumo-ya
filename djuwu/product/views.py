668312b44eabb754d46cd68f9e9964397f9e34c6
mion158 <111487792+mion158@users.noreply.github.com>
2023-02-25 15:30:21 +0000

    added view category backend

--- djuwu/product/views.py
+++ djuwu/product/views.py
@@ -3,8 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import Http404
 
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Category
+from .serializers import CategorySerializer, ProductSerializer
 
 # Create your views here.
 class LatestProductsList(APIView):
@@ -24,7 +24,19 @@ class ProductDetail(APIView):
 
     # overwrite get function
     def get(self, request, category_slug, product_slug, format=None):
-        product = self.get_object(category_slug, product)
+        product = self.get_object(category_slug, product_slug)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
-        
+
+class CategoryDetail(APIView):
+    def get_object(self, category_slug):
+        try:
+            return Category.objects.get(slug=category_slug)
+        except Category.DoesNotExist:
+            raise Http404
+
+    # overwrite get function
+    def get(self, request, category_slug, format=None):
+        category = self.get_object(category_slug)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
